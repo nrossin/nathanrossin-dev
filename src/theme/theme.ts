6f0241886bba320859e0e38b5bb3a6ef1c83aae2
nathan.rossin <herblock@protonmail.com>
2025-04-30 23:44:45 -0400

    Update gradients; boring, but serviceable

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -48,11 +48,9 @@ const darkPalette = {
     },
     divider: '#1e1e1e',
     sectionBackgrounds: {
-        home: 'linear-gradient(320.54deg, #00069F 0%, #120010 72.37%), linear-gradient(58.72deg, #69D200 0%, #970091 100%), linear-gradient(121.28deg, #8CFF18 0%, #6C0075 100%), linear-gradient(121.28deg, #8000FF 0%, #000000 100%), linear-gradient(180deg, #00FF19 0%, #24FF00 0.01%, #2400FF 100%), linear-gradient(52.23deg, #0500FF 0%, #FF0000 100%), linear-gradient(121.28deg, #32003A 0%, #FF4040 100%), radial-gradient(50% 72.12% at 50% 50%, #EB00FF 0%, #110055 100%);' + 'background-blend-mode: screen, color-dodge, color-burn, screen, overlay, difference, color-dodge, normal;',
-        resume: 'linear-gradient(115deg, #000000 0%, #00C508 55%, #000000 100%), linear-gradient(115deg, #0057FF 0%, #020077 100%), conic-gradient(from 110deg at -5% 35%, #000000 0deg, #FAFF00 360deg), conic-gradient(from 220deg at 30% 30%, #FF0000 0deg, #0000FF 220deg, #240060 360deg), conic-gradient(from 235deg at 60% 35%, #0089D7 0deg, #0000FF 180deg, #240060 360deg);\n' +
-            'background-blend-mode: soft-light, soft-light, overlay, screen, normal;',
-        projects: 'radial-gradient(23.53% 23.53% at 26.47% 77.52%, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(32.26% 32.26% at 86.54% 64.29%, rgba(255, 255, 255, 0.2) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(44.89% 44.89% at 60.6% 20%, rgba(0, 14, 116, 0.48) 0%, rgba(0, 0, 0, 0) 100%), linear-gradient(53.86deg, rgba(255, 255, 255, 0.2) 29.08%, rgba(0, 0, 0, 0) 64.4%), linear-gradient(231.06deg, #010103 15.27%, #00107D 34.07%, #3174B1 47%, #AAB2BC 64.23%, #BEB3BD 72.85%, #BBADB8 90.47%);\n' +
-            'background-blend-mode: overlay, screen, overlay, screen, normal;\n',
+        home: 'linear-gradient(to right, rgb(55, 65, 81), rgb(17, 24, 39), rgb(0, 0, 0));',
+        resume: 'linear-gradient(147deg, #000000 0%, #2c3e50 74%);',
+        projects: 'linear-gradient(315deg, #003153 0%, #1B1B1B 74%);\n',
         contact: 'radial-gradient(140% 107% at 50% 10%, #000314 37.41%, #6633EE 69.27%, #FFFFFF 100%);'
     },
 };
